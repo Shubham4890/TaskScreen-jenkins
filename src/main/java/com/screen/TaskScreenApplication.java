4a7531af31721220231b8dcbb08325fc6c3073ae
Shubham4890 <shubham.sharma@trangile.com>
2023-11-29 15:40:49 +0530

    Taskpush

--- src/main/java/com/screen/TaskScreenApplication.java
+++ src/main/java/com/screen/TaskScreenApplication.java
@@ -2,9 +2,17 @@ package com.screen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TaskScreenApplication {
+public class TaskScreenApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(TaskScreenApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskScreenApplication.class, args);
